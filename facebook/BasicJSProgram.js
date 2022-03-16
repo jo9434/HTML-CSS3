f7db25eb13a8a8929b56c6e84975d2a07c87ab1e
Joel <91497556+jo9434@users.noreply.github.com>
2022-03-16 15:47:11 +0530

    Add files via upload

---
+++ facebook/BasicJSProgram.js
@@ -0,0 +1,61 @@
+function notifyUser1()
+{
+    var notifyUser1 = document.getElementById("btn-message1");
+    notifyUser1.style.color = "#9642F5";
+    notifyUser1.style.height = "19px";
+    notifyUser1.style.width = "22px";
+    return notifyUser1;
+}
+
+function disableUser1()
+{
+    var disableUser1 = document.getElementById("btn-message1");
+    disableUser1.style.color = "white";
+    disableUser1.style.height = "15px";
+    disableUser1.style.width = "15px";
+    return disableUser1;
+}
+
+function notifyUser2()
+{
+    var notifyUser2 = document.getElementById("btn-message2");
+    notifyUser2.style.color = "#42F55A";
+    notifyUser2.style.height = "19px";
+    notifyUser2.style.width = "22px";
+    return notifyUser2;
+}
+
+function disableUser2()
+{
+    var disableUser2 = document.getElementById("btn-message2");
+    disableUser2.style.color = "white";
+    disableUser2.style.height = "15px";
+    disableUser2.style.width = "15px";
+    return disableUser2;
+}
+
+function notifyUser3()
+{
+    var notifyUser3 = document.getElementById("btn-message3");
+    notifyUser3.style.color = "#F59E42";
+    notifyUser3.style.height = "19px";
+    notifyUser3.style.width = "22px";
+    return notifyUser3;
+}
+
+function disableUser3()
+{
+    var disableUser3 = document.getElementById("btn-message3");
+    disableUser3.style.color = "white";
+    disableUser3.style.height = "15px";
+    disableUser3.style.width = "15px";
+    return disableUser3;
+}
+
+function notifyIcon()
+{
+    var notifyIcon1 = document.getElementById("btn-message1").insertAdjacentText = "1";
+    var notifyIcon2 = document.getElementById("btn-message2").insertAdjacentText = "4";
+    var notifyIcon3 = document.getElementById("btn-message3").insertAdjacentText = "7";
+    return notifyIcon1, notifyIcon2, notifyIcon3;
+}
